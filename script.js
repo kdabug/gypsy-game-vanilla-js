1deb19f99a38cc910234b26f427c978e255c4587
Mikayda Mills <mills.ma12@gmail.com>
2019-02-04 01:13:33 -0500

    started logic for tarot card option

--- script.js
+++ script.js
@@ -590,3 +590,72 @@ createGemObject("clearTopaz", "clearTopaz", "white", "");
 
 console.log(gemObjs.length);
 gemObjs.forEach(el => placeGems(el));
+
+const pickGem = () => {
+  const gemId = this.getAttribute("data.id");
+};
+//ADDING LOGIC FOR THE TAROT CARD GAME
+
+const tarotDeck = [];
+
+const addToDeck = (name, number, suit, meaning, image) => {
+  tarotDeck.push({
+    name,
+    number,
+    suit,
+    meaning,
+    image
+  });
+};
+
+addToDeck(
+  "the fool",
+  0,
+  "major arcana",
+  "new beginnings, optimism, trust in life"
+);
+addToDeck("the magician", 1, "major arcana", "");
+addToDeck("the high priestess", 2, "major arcana", "");
+addToDeck("the empress", 3, "major arcana", "");
+addToDeck("the emperor", 4, "major arcana", "");
+addToDeck("the heirophant", 5, "major arcana", "");
+addToDeck("the lovers", 6, "major arcana", "");
+addToDeck("the chariot", 7, "major arcana", "");
+addToDeck("strength", 8, "major arcana", "");
+addToDeck("the hermit", 9, "major arcana", "");
+addToDeck("wheel of fortune", 10, "major arcana", "");
+addToDeck("justice", 11, "major arcana", "");
+addToDeck("the hanged man", 12, "major arcana", "");
+addToDeck("death", 13, "major arcana", "");
+addToDeck("temperance", 14, "major arcana", "");
+addToDeck("the devil", 15, "major arcana", "");
+addToDeck("the tower", 16, "major arcana", "");
+addToDeck("the star", 17, "major arcana", "");
+addToDeck("the moon", 18, "major arcana", "");
+addToDeck("the sun", 19, "major arcana", "");
+addToDeck("judgement", 20, "major arcana", "");
+addToDeck("the world", 21, "major arcana", "");
+addToDeck("ace", 1, "wands", "");
+addToDeck("two", 2, "wands", "");
+addToDeck("three", 3, "wands ", "");
+addToDeck("four", 4, "wands", "");
+addToDeck("five", 5, "wands", "");
+addToDeck("six", 6, "wands", "");
+addToDeck("seven", 7, "wands", "");
+addToDeck("eight", 8, "wands", "");
+addToDeck("nine", 9, "wands", "");
+addToDeck("ten", 10, "wands", "");
+addToDeck("page", 11, "wands", "");
+addToDeck("prince", 12, "wands", "");
+addToDeck("queen", 13, "wands", "");
+addToDeck("king", 14, "wands", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
+addToDeck("fool", 0, "swords", "");
