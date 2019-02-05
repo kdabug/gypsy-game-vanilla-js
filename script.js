5d1b688917f60ab915c5de2ec3e10481a388ee62
Mikayda Mills <mills.ma12@gmail.com>
2019-02-04 22:18:17 -0500

    drafted logic for three number choices

--- script.js
+++ script.js
@@ -5,21 +5,14 @@ const rocks = document.querySelector(".rock");
 const leaves = document.querySelector(".leaf");
 const gems = document.querySelector(".gem");
 const gypsy = document.querySelector(".wait-gypsy");
-const text = document.querySelector(".text");
+const instructions = document.querySelector(".instructions");
 
 //logic for gems and gem counter
 const gemNameText = document.createElement("h2");
 gemNameText.innerText = ``;
-text.appendChild(gemNameText);
+instructions.appendChild(gemNameText);
 const addGemCounter = document.createElement("h2");
 addGemCounter.innerText = "Gems: 0";
-timer.appendChild(addGemCounter);
-
-//TIMER AND COUNTDOWN
-const timer = document.querySelector(".timer");
-const createTimer = document.createElement("h2");
-createTimer.innerText = "";
-timer.appendChild(createTimer);
 
 //Starting Conditions
 let gemObjs = [];
@@ -27,11 +20,18 @@ let returnedGemObjs = [];
 let gemCount = 0;
 let countDown = 20;
 let lowestScore = 15;
+let lastPickedNumber = -1;
+let firstPickedNumber = -1;
+let secondPickedNumber
+
+//TIMER AND COUNTDOWN
+const timer = document.querySelector(".timer");
+const createTimer = document.createElement("h2");
+createTimer.innerText = `Timer: ${countDown}`;
+timer.appendChild(createTimer);
+timer.appendChild(addGemCounter);
 
 //////FUNCTIONS
-
-// countDownTimer takes countDown and starts a countdown timer
-//turns Gypsy Red
 
 const restartButton = element => {
   const createRestart = document.createElement("button");
@@ -43,38 +43,76 @@ const restartButton = element => {
     document.location.reload();
   });
 };
+
+const createNumberForm = (place) => {
+  askForANumber = document.createElement("INPUT");
+  askForANumber.id = "form";
+  askForANumber.setAttribute("type", "number");
+  askForANumber.setAttribute("placeholder", place);
+  gypsy.appendChild(askForANumber);
+}
+
+//turns Gypsy Red
 const gypsyTurnsRed = () => {
-  setTimeout(function() {
-    gypsy.id = "changed";
-    console.log(gypsy.id);
-  }, 500);
+  gypsy.id = "changed";
   const meanGypsyText = document.createElement("h2");
   meanGypsyText.innerText =
     "STOP THIEF! /n POLICE!! THIS TRAVELER IS STEALING MY PRECIOUS GEMS!";
-  gypsy.appendChild("text");
+  gypsy.appendChild(meanGypsyText);
   setTimeout(function() {
     meanGypsyText.innerText = "";
     restartButton(gypsy);
     setTimeout(function() {
       document.location.reload();
-    } 10000);
+    }, 10000);
   }, 2000);
 };
 
-//turns Gypsy Normal
-const gypsyTurnsNormal = () => {
-  setTimeout(function() {
-    gypsy.id = "gypsy-picture";
-    console.log(gypsy.id);
-  }, 500);
-};
-
 const winningPicks = () => {
-  
+  const winningText = document.createElement("h1");
+  gypsy.appendChild(winningText);
+  winningText.innerText = `Thank you, kind traveler, for helping me. ... ... ... I'm feeling impressed to tell you coming thing. Can you please give me a number between 1 and ${
+    returnedGemObjs.length
+  }.`;
+  createNumberForm("first number")
+  let pickedNumber = document.querySelector('#form').value;
+  if (pickedNumber !== lastPickedNumber && pickedNumber > 0 && pickedNumber <= returnedGemObjs.length) {
+    winningText.innerText = `Ahh yes. The ${returnedGemObjs[pickedNumber-1].stone} is such a beautiful ${returnedGemObjs[pickedNumber-1].color}. It's telling me your past was filled with ${returnedGemObjs[pickedNumber-1].heals}.`;
+    firstPickedNumber = pickedNumber;
+    gypsy.removeChild('#form');
+  } else {
+    winningText.innerText = `Please pick a number between 1 and ${returnedGemObjs.length
+  }`;
+  }
+  winningText.innerText = `Now for your present. Give me a different number between 1 and ${
+    returnedGemObjs.length
+  }.`;
+  createNumberForm("second number");
+  if (pickedNumber !== firstPickedNumber && pickedNumber > 0 && pickedNumber <= returnedGemObjs.length) {
+    winningText.innerText = `Your present speaks to the ${returnedGemObjs[pickedNumber-1].color} ${returnedGemObjs[pickedNumber-1].stone} signifying ${returnedGemObjs[pickedNumber].heals}.`;
+    secondPickedNumber = pickedNumber
+    gypsy.removeChild('#form');
+  } else {
+    winningText.innerText = `Please pick a number between 1 and ${returnedGemObjs.length
+  }`;
+  winningText.innerText = `Now, give me a different number between 1 and ${
+    returnedGemObjs.length
+  }.`;
+  createNumberForm("final number")
+  if (pickedNumber !== firstPickedNumber && pickedNumber !== secondPickedNumber && pickedNumber > 0 && pickedNumber <= returnedGemObjs.length) {
+    winningText.innerText = `Your futures bodes of ${returnedGemObjs[pickedNumber-1].heals}. This requires further study of the ${returnedGemObjs[pickedNumber-1].color} ${returnedGemObjs[pickedNumber-1].stone}.`;
+    lastPickedNumber = pickedNumber
+    gypsy.removeChild('#form');
+  } else {
+    winningText.innerText = `Please pick a number between 1 and ${returnedGemObjs.length
+  }`;
+  }
 };
 
-const countDownTimer = setInterval(function() {
-  if (countDown <= 0) {
+
+// countDownTimer takes countDown and starts a countdown timer
+const countDownTimer = () => {
+  if (countDown < 0) {
     clearInterval(countDownTimer);
     alert(`You collected ${gemCount} gems.`);
     ground.style.display = "none";
@@ -84,16 +122,21 @@ const countDownTimer = setInterval(function() {
       gypsyTurnsRed();
     }
   }
-  if (countDown > 0) {
-    createTimer.innertext = `Timer: ${countDown}`;
+  if (countDown >= 0) {
+    createTimer.innerText = `Timer: ${countDown}`;
   }
   countDown--;
-}, 1000);
+}
+
+const endScene = () => {
+  gypsy.style.display = 'none';
+  
+}
 
 //
 const randomPosition = item => {
   item.style.left = Math.random() * window.innerWidth + "px";
-  item.style.top = Math.random() * window.innerHeight - 100 + "px";
+  item.style.top = Math.random() * window.innerHeight - 200 + "px";
 };
 
 //pickUpItem takes an id and add anevent listener to picked up items
@@ -104,19 +147,19 @@ const pickUpItem = item => {
     gemNameText.innerText = `Oh no! You've picked up a ${
       item.id
     } and lost a gem.`;
-    count--;
-    addGemCounter.innerText = `Gems: ${count}`;
+    gemCount--;
+    addGemCounter.innerText = `Gems: ${gemCount}`;
     document.querySelector(".picked").remove();
   });
 };
 
 //creates a random position for the rocks and leaves
-const placeItems = (n, item, url, selector, id) => {
+const placeItems = (n, url, selector, id) => {
   for (let i = 0; i < n; i++) {
     const newDiv = document.createElement("div");
     item = document.createElement("img");
     item.setAttribute("src", url);
-    item.setAttribute("id", id);
+    item.classList.add(id);
     randomPosition(item);
     newDiv.appendChild(item);
     pickUpItem(item);
@@ -126,7 +169,6 @@ const placeItems = (n, item, url, selector, id) => {
 
 placeItems(
   3,
-  "largeRock",
   "https://images.vexels.com/media/users/3/145827/isolated/preview/357f06ecbaaa77d750259c459c0ed55f-round-rock-illustration-by-vexels.png",
   rocks,
   "rock"
@@ -134,7 +176,6 @@ placeItems(
 
 placeItems(
   3,
-  "smallRock",
   "https://images.vexels.com/media/users/3/145827/isolated/preview/357f06ecbaaa77d750259c459c0ed55f-round-rock-illustration-by-vexels.png",
   rocks,
   "rock"
@@ -142,7 +183,6 @@ placeItems(
 
 placeItems(
   4,
-  "largeRedLeaf",
   "http://res.freestockphotos.biz/pictures/16/16812-illustration-of-an-orange-autumn-leaf-pv.png",
   leaves,
   "leaf"
@@ -150,7 +190,6 @@ placeItems(
 
 placeItems(
   3,
-  "smallRedLeaf",
   "http://res.freestockphotos.biz/pictures/16/16813-illustration-of-an-orange-autumn-leaf-pv.png",
   leaves,
   "leaf"
@@ -158,7 +197,6 @@ placeItems(
 
 placeItems(
   2,
-  "greenLeaf",
   "http://www.lindicepensable.ch/wordpress/wp-content/uploads/2018/07/Feuille-de-vigne-avec-insecte.png",
   leaves,
   "leaf"
@@ -182,8 +220,8 @@ const pickUpGem = id => {
       if (gemName === gemObjs[i].id) {
         returnedGemObjs.push(gemObjs[i]);
         gemNameText.innerText = `You've picked up the ${gemName}.`;
-        count++;
-        addGemCounter.innerText = `Gems: ${count}`;
+        gemCount++;
+        addGemCounter.innerText = `Gems: ${gemCount}`;
       }
     }
     document.querySelector(".picked").remove();
@@ -717,7 +755,7 @@ gemObjs.forEach(el => placeGems(el));
 
 const groundPlayPickUp = () => {
   ground.style.display = "show";
-  countDownTimer();
+  setInterval(countDownTimer, 1000);
 };
 
 const playGame = () => {
@@ -731,8 +769,6 @@ const playGame = () => {
 };
 
 playGame();
-
-gypsy.addEventListener("click", gypsyTurnsRed);
 
 //ADDING LOGIC FOR THE TAROT CARD GAME
 
